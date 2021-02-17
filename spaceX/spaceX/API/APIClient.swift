feb0cbdcdfc91f6c8eb835181b027c73a129572f
marcoceles <marco.celestino@ovolab.com>
2021-02-17 19:43:03 +0100

    created APIClient and defined method to get rockets list

---
+++ spaceX/spaceX/API/APIClient.swift
@@ -0,0 +1,38 @@
+//
+//  APIClient.swift
+//  spaceX
+//
+//  Created by Marco Celestino on 17/02/2021.
+//
+
+import Foundation
+import Alamofire
+
+class APIClient{
+
+    public static let shared = APIClient()
+
+    private init(){}
+
+    func makeAPICall<O : Decodable>(_ endpoint: APIEndpoint, responseClass: O.Type, decoder: JSONDecoder, completion: @escaping (Result<O, AFError>) -> Void) -> Void {
+        let request = AF.request(endpoint, method: endpoint.method)
+        request.responseDecodable { (response) in
+            completion(response.result)
+        }
+    }
+}
+
+extension APIClient{
+
+    public func getRockets(_ completion: @escaping (Result<[Rocket], Error>) -> Void){
+        self.makeAPICall(.getRockets, responseClass: [Rocket].self, decoder: JSONDecoder(), completion: { result in
+
+            switch result{
+            case .success(let car):
+                completion(.success(car))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
